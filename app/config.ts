301de521f88e6d26152ab175694ff613438aea01
Fernando Zavalia <zavaliafernando@gmail.com>
2021-03-17 14:39:22 -0300

    Remove component

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,17 @@
-export type ReviewConfig = { name: string; img: string };
+export type ReviewConfig = { name: string; img: string; author: string };
 
 export type Config = { reviews: Map<string, ReviewConfig> };
 
 const config: Config = {
   reviews: new Map([
-    ["the-long-walk", { name: "The Long Walk", img: "/the-long-walk.jpg" }],
+    [
+      "the-long-walk",
+      {
+        name: "The Long Walk",
+        img: "/the-long-walk.jpg",
+        author: "Stephen King (as Richard Bachman)",
+      },
+    ],
   ]),
 };
 
