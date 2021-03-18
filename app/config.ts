6b7b799678a9ce33dcf40ecd66591997c51e8304
Fernando Zavalia <zavaliafernando@gmail.com>
2021-03-17 21:59:19 -0300

    book list

--- app/config.ts
+++ app/config.ts
@@ -1,18 +1,27 @@
-export type ReviewConfig = { name: string; img: string; author: string };
+export type BookConfig = {
+  name: string;
+  img: string;
+  author: string;
+};
+type ISBN = string;
 
-export type Config = { reviews: Map<string, ReviewConfig> };
+export type Config = { books: ISBN[] };
 
 const config: Config = {
-  reviews: new Map([
-    [
-      "the-long-walk",
-      {
-        name: "The Long Walk",
-        img: "/the-long-walk.jpg",
-        author: "Stephen King (as Richard Bachman)",
-      },
-    ],
-  ]),
+  books: [
+    "0141036141", // 1984
+    "198211598X", // Pet Samatary,
+    "0316212296", // The Red Knight
+    "1982110589", // The Institute
+    "0006480411", // Neuromancer
+    "1416513450", // Pegasus Descending
+    "0465050654", // Design of everyday things
+    "0575079797", // The blade itself
+    "0575077883", // Before they are hanged
+    "1591026903", // Last argument of kings
+    "1260026671", // Cryptoassets
+    "076531178X", // Final empire
+  ],
 };
 
 export default config;
