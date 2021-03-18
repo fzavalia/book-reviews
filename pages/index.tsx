a2cd46ca69f5591f58300608cfed102243ccc533
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-03-18 13:41:39 -0300

    Book grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,18 @@ type HomeProps = {
 
 const Home = ({ books }: HomeProps) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 max-w-max gap-3 mx-auto">
-      {books.map((book) => (
-        <img className="h-full shadow-md" src={book.img}></img>
-      ))}
+    <div>
+      <div className="text-3xl text-center mt-16 sm:mt-32 mb-16 sm:mb-32">
+        Book Reviews
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 max-w-max gap-3 mx-auto">
+        {books.map((book) => (
+          <img
+            className="h-full shadow-md bg-white rounded-lg p-1 bg-opacity-50 cursor-pointer transform hover:scale-105 transition-transform ease-linear duration-100"
+            src={book.img}
+          ></img>
+        ))}
+      </div>
     </div>
   );
 };
