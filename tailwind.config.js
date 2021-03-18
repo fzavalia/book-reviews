e7cbedeed8d2d9a9d8d7f24a67612df7ac8f3660
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-03-18 16:17:02 -0300

    Add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './app/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "forest-dark": "url('/bg-misty-forest-dark.jpg')",
+        "forest-light": "url('/bg-misty-forest.jpg')",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ["dark"],
+    },
   },
   plugins: [],
-}
+};
